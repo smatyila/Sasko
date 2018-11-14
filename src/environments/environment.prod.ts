bd7f4d7984c6e3aaaedcdbd5903f9ece0284a120
A231049 <simthembile.yuku@standardbank.co.za>
2018-11-14 21:59:32 +0200

    Fixing lint failers

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDWNVNTXuk-SUdF0un9h9Ux96Vpq8xZC30",
-    authDomain: "saskoandroid-93176.firebaseapp.com",
-    databaseURL: "https://saskoandroid-93176.firebaseio.com",
-    projectId: "saskoandroid-93176",
-    storageBucket: "saskoandroid-93176.appspot.com",
-    messagingSenderId: "105105935749"
+    apiKey: 'AIzaSyDWNVNTXuk-SUdF0un9h9Ux96Vpq8xZC30',
+    authDomain: 'saskoandroid-93176.firebaseapp.com',
+    databaseURL: 'https://saskoandroid-93176.firebaseio.com',
+    projectId: 'saskoandroid-93176',
+    storageBucket: 'saskoandroid-93176.appspot.com',
+    messagingSenderId: '105105935749'
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,12 +6,12 @@ export const environment = {
   production: false,
   // Initialize Firebase
   firebase: {
-    apiKey: "AIzaSyDWNVNTXuk-SUdF0un9h9Ux96Vpq8xZC30",
-    authDomain: "saskoandroid-93176.firebaseapp.com",
-    databaseURL: "https://saskoandroid-93176.firebaseio.com",
-    projectId: "saskoandroid-93176",
-    storageBucket: "saskoandroid-93176.appspot.com",
-    messagingSenderId: "105105935749"
+    apiKey: 'AIzaSyDWNVNTXuk-SUdF0un9h9Ux96Vpq8xZC30',
+    authDomain: 'saskoandroid-93176.firebaseapp.com',
+    databaseURL: 'https://saskoandroid-93176.firebaseio.com',
+    projectId: 'saskoandroid-93176',
+    storageBucket: 'saskoandroid-93176.appspot.com',
+    messagingSenderId: '105105935749'
   }
 };
 
